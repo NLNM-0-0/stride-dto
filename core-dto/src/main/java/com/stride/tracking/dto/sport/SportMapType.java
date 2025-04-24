ed15bf28570f3b96226ea89b8d277a5c87961479
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-04-24 09:05:46 +0700

    dev: add lowercase for SportMapType

--- core-dto/src/main/java/com/stride/tracking/dto/sport/SportMapType.java
+++ core-dto/src/main/java/com/stride/tracking/dto/sport/SportMapType.java
@@ -1,7 +1,17 @@
 package com.stride.tracking.dto.sport;
 
 public enum SportMapType {
-    DRIVING,
-    WALKING,
-    CYCLING
+    DRIVING("driving"),
+    WALKING("walking"),
+    CYCLING("cycling");
+
+    private final String lowercase;
+
+    SportMapType(String lowercase) {
+        this.lowercase = lowercase;
+    }
+
+    public String getLowercase() {
+        return lowercase;
+    }
 }
