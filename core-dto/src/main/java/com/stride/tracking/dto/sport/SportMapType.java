5320b026ebd975cae05484b0e1e729a5f3c36a68
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-05-05 14:26:08 +0700

    dev: Add recordMeters and recommendedDistance to SportMapType

--- core-dto/src/main/java/com/stride/tracking/dto/sport/SportMapType.java
+++ core-dto/src/main/java/com/stride/tracking/dto/sport/SportMapType.java
@@ -1,17 +1,16 @@
 package com.stride.tracking.dto.sport;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum SportMapType {
-    DRIVING("driving"),
-    WALKING("walking"),
-    CYCLING("cycling");
+    DRIVING("driving", 10, 1000),
+    WALKING("walking", 1, 100),
+    CYCLING("cycling", 10, 1000);
 
     private final String lowercase;
-
-    SportMapType(String lowercase) {
-        this.lowercase = lowercase;
-    }
-
-    public String getLowercase() {
-        return lowercase;
-    }
+    private final int recordMeters;
+    private final int recommendedDistance;
 }
