ce5e091d6535eaa98657c0bbc2a798753632230a
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-05-05 17:20:47 +0700

    fix: add Data, Builder, NoArgsConstructor, AllArgsConstructor to HeartRateZoneResponse

--- core-dto/src/main/java/com/stride/tracking/dto/activity/response/HeartRateZoneResponse.java
+++ core-dto/src/main/java/com/stride/tracking/dto/activity/response/HeartRateZoneResponse.java
@@ -1,5 +1,14 @@
 package com.stride.tracking.dto.activity.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class HeartRateZoneResponse {
     private String zoneId;
     private String name;
