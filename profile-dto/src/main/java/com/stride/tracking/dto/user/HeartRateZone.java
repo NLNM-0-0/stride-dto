7779eecfd190fbe2235a9fd4f55774e80783d494
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-05-05 17:14:09 +0700

    update: add more info to HeartRateZone

--- profile-dto/src/main/java/com/stride/tracking/dto/user/HeartRateZone.java
+++ profile-dto/src/main/java/com/stride/tracking/dto/user/HeartRateZone.java
@@ -1,5 +1,18 @@
 package com.stride.tracking.dto.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum HeartRateZone {
-    ZONE1, ZONE2, ZONE3, ZONE4, ZONE5
+    ZONE1("1", "Endurance"),
+    ZONE2("2", "Moderate"),
+    ZONE3("3", "Tempo"),
+    ZONE4("4", "Threshold"),
+    ZONE5("5", "Anaerobic"),
+    ;
+
+    private final String id;
+    private final String name;
 }
