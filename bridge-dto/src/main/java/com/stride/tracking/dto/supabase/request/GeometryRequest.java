f0f125525834e9ef12a4cf4d0efb50e07b182058
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-05-06 10:02:06 +0700

    dev: add GetLocationByGeometryRequest

---
+++ bridge-dto/src/main/java/com/stride/tracking/dto/supabase/request/GeometryRequest.java
@@ -0,0 +1,17 @@
+package com.stride.tracking.dto.supabase.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GeometryRequest {
+    private String type;
+    private List<List<Double>> coordinates;
+}
