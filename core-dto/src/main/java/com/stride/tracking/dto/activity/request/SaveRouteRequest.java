2a224b31c9bdc9dff5d94481db8bac966981be6e
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-05-11 17:32:52 +0700

    dev: add routeName when saving route

---
+++ core-dto/src/main/java/com/stride/tracking/dto/activity/request/SaveRouteRequest.java
@@ -0,0 +1,14 @@
+package com.stride.tracking.dto.activity.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaveRouteRequest {
+    private String routeName;
+}
