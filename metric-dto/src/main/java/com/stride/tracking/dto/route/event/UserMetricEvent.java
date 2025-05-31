be1c7252d14e37474616f73927c112ed85d995c5
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-05-31 23:02:04 +0700

    dev: add UserMetricEvent

---
+++ metric-dto/src/main/java/com/stride/tracking/dto/route/event/UserMetricEvent.java
@@ -0,0 +1,20 @@
+package com.stride.tracking.dto.route.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserMetricEvent {
+    private String userId;
+
+    private Instant time;
+
+    private String provider;
+}
