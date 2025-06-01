b8d5236c8f82dde7d611a7e1c56dc02abdd80112
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-06-01 16:48:29 +0700

    dev: add CreateUserIdentityRequest

---
+++ identity-dto/src/main/java/com/stride/tracking/identity/dto/user/request/CreateUserIdentityRequest.java
@@ -0,0 +1,16 @@
+package com.stride.tracking.identity.dto.user.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateUserIdentityRequest {
+    private String email;
+    private String password;
+    private Boolean isAdmin;
+}
