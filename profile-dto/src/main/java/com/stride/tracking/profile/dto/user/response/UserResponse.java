320df882cbe27c9aa833beaed9e2407d75022cb0
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-06-02 18:40:53 +0700

    dev: add dob to UserResponse

--- profile-dto/src/main/java/com/stride/tracking/profile/dto/user/response/UserResponse.java
+++ profile-dto/src/main/java/com/stride/tracking/profile/dto/user/response/UserResponse.java
@@ -14,6 +14,7 @@ public class UserResponse {
     private String name;
     private String email;
     private String ava;
+    private String dob;
     private Boolean isAdmin;
     private Boolean isBlocked;
 }
