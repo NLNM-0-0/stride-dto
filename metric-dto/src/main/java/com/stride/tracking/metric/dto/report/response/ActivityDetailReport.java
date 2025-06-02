cfc073e94963204b690420e69cf4b9a38990a540
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-06-03 06:59:30 +0700

    fix: use double for avgHeartRate

--- metric-dto/src/main/java/com/stride/tracking/metric/dto/report/response/ActivityDetailReport.java
+++ metric-dto/src/main/java/com/stride/tracking/metric/dto/report/response/ActivityDetailReport.java
@@ -16,5 +16,5 @@ public class ActivityDetailReport {
     private long elevationGain;
     private long distance;
     private int calories;
-    private int avgHeartRate;
+    private double avgHeartRate;
 }
