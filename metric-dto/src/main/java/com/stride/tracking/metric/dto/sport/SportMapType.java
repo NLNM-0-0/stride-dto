f4b525e139d3bfff49f389d9e31c7ab6587610f7
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-06-03 20:38:22 +0700

    dev: add minDistance to SportMapType

--- metric-dto/src/main/java/com/stride/tracking/metric/dto/sport/SportMapType.java
+++ metric-dto/src/main/java/com/stride/tracking/metric/dto/sport/SportMapType.java
@@ -6,12 +6,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum SportMapType {
-    DRIVING("driving", 10, 1000),
-    WALKING("walking", 1, 100),
-    CYCLING("cycling", 10, 1000),
-    NO_MAP(null, 0, 0);
+    DRIVING("driving", 10, 1000, 100),
+    WALKING("walking", 1, 100, 50),
+    CYCLING("cycling", 10, 1000, 100),
+    NO_MAP(null, 0, 0, 0);
 
     private final String lowercase;
     private final int recordMeters;
     private final int recommendedDistance;
+    private final int minDistance;
 }
