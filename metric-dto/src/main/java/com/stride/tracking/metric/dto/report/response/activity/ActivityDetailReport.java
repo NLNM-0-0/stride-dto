a6ad4c225b83dbf3520e0c94cc2ea204bf74a50e
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-06-03 14:04:04 +0700

    update: use SportDetailReport for ActivityDetailReport

--- metric-dto/src/main/java/com/stride/tracking/metric/dto/report/response/activity/ActivityDetailReport.java
+++ metric-dto/src/main/java/com/stride/tracking/metric/dto/report/response/activity/ActivityDetailReport.java
@@ -1,6 +1,6 @@
 package com.stride.tracking.metric.dto.report.response.activity;
 
-import com.stride.tracking.metric.dto.sport.response.SportShortResponse;
+import com.stride.tracking.metric.dto.report.response.sport.SportDetailReport;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ActivityDetailReport {
     private String id;
-    private SportShortResponse sport;
+    private SportDetailReport sport;
     private long time;
     private long elevationGain;
     private long distance;
