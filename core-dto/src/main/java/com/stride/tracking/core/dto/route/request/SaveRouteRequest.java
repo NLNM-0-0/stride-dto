542e29a3115aebfe6438e94f2d06eeb1d283d425
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-06-06 09:31:08 +0700

    fix: make route name not required

--- core-dto/src/main/java/com/stride/tracking/core/dto/route/request/SaveRouteRequest.java
+++ core-dto/src/main/java/com/stride/tracking/core/dto/route/request/SaveRouteRequest.java
@@ -1,6 +1,5 @@
 package com.stride.tracking.core.dto.route.request;
 
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,7 +12,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SaveRouteRequest {
 
-    @NotNull(message = "Route name must not be null")
     @Size(min = 1, message = "Route name must not be empty")
     private String routeName;
 }
